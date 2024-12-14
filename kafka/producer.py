f247a4912717763550705fc9e07b750a6aa0f035
amadheatrizza <amadheatrizza@gmail.com>
2024-12-15 04:49:09 +0700

    update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,72 +1,60 @@
 import json
 from kafka import KafkaProducer
 import time
-import os
-import utils
+import requests
 
-API_KEY = os.getenv("API_KEY")
-CATEGORIES = ["business", "entertainment", "general", 
-              "health", "science", 
-              "sports", "technology"]
-COUNTRIES = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch',
-             'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 
-             'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 
-             'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 
-             'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 
-             'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za']
-
-TOPIC = 'news-topic'
+ROOT = "https://environment.data.gov.uk/flood-monitoring"
+TOPIC = 'floods-topic'
 
 producer = KafkaProducer(
     bootstrap_servers="localhost:29092",
+    api_version=(2,0,2),
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-sent_article_urls = set()
+sent_floods_data = set()
 
-def process_and_send_articles(articles, category=None, country=None):
-    for article in articles:
-        try:
-            article_url = article['url']
-            if article_url in sent_article_urls:
-                print(f"Duplicate article found: {article_url}, skipping.")
-                continue
-            
-            sent_article_urls.add(article_url)
-            message = {
-                "category": category,
-                "country": country,
-                "title": article['title'],
-                "content": article['content'],
-                "publisher": article['source']['name'],
-                "publishedAt": article['publishedAt'],
-                "url": article['url']
-            }
+def extract_id_from_uri(uri):
+    return uri.split('/')[-1] if uri else None
 
-            producer.send(TOPIC, value=message)
-            print('Sent article:', message)
+def process_and_send_data(items):
+    for item in items:
+        flood_id = extract_id_from_uri(item.get('@id', ''))
 
-        except Exception as e:
-            print(f"Error processing article: {e}")
+        if (flood_id in sent_floods_data):
+            continue
+
+        sent_floods_data.add(flood_id)
+    
+        message = {
+            "id": flood_id,
+            "description": item.get("description"),
+            "ea_area": item.get("eaAreaName"),
+            "ea_region": item.get("eaRegionName"),
+            "county": item.get("floodArea", {}).get("county"),
+            "notation": item.get("floodArea", {}).get("notation"),
+            "river_or_sea": item.get("floodArea", {}).get("riverOrSea"),
+            "polygon_url": item.get("floodArea", {}).get("polygon"),
+            "severity": item.get("severity"),
+            "severity_level": item.get("severityLevel"),
+            "is_tidal": item.get("isTidal"),
+            "time_message_changed": item.get("timeMessageChanged"),
+            "time_raised": item.get("timeRaised"),
+            "time_severity_changed": item.get("timeSeverityChanged")
+        }
+
+        producer.send(TOPIC, value=message)
+        print("SENT", message)
 
 def fetch_and_send():
-    for category in CATEGORIES:
-        print(f"Fetching articles for category: {category}")
-        articles = utils.fetch_data_by_category(api_key=API_KEY, category=category)
-        if articles:
-            process_and_send_articles(articles, category=category)
-        else:
-            print(f"No articles found for category: {category}")
+    res = requests.get(f"{ROOT}/id/floods")
 
-    for country in COUNTRIES:
-        print(f"Fetching articles for country: {country}")
-        articles = utils.fetch_data_by_category(api_key=API_KEY, country=country)
-        if articles:
-            process_and_send_articles(articles, country=country)
-        else:
-            print(f"No articles found for country: {country}")
+    if (res.status_code == 200):
+        process_and_send_data(res.json().get("items"))
+    else:
+        print("Fail to fetch API")
 
 while True:
     fetch_and_send()
     print("Batch sent to Kafka.")
-    time.sleep(60) 
+    time.sleep(60)
