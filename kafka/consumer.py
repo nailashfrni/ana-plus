e40721fe4dc70271288c80055eae9bd504d8c982
agnestsiburian <siburian.agnestheresia@gmail.com>
2024-12-14 09:13:30 +0700

    Update consumer.py

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -3,13 +3,36 @@ from elasticsearch import Elasticsearch
 import json
 
 TOPIC = "news-topic"
-es = Elasticsearch([{"host": "elasticsearch", "port": 9200}])
+# es = Elasticsearch([{"host": "elasticsearch", "port": 9200}])
 
-consumer = KafkaConsumer(TOPIC, bootstrap_servers="kafka:9092", auto_offset_reset="earliest", value_deserializer=lambda x: x.decode("utf-8"))
+consumer = KafkaConsumer(
+    TOPIC,
+    bootstrap_servers='localhost:29092',
+    value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+    auto_offset_reset='earliest',  # Start reading messages from the beginning
+    enable_auto_commit=True  # Automatically commit offsets
+)
 
-def index_data(document):
-    es.index(index="news-index", body=json.loads(document))
+def process_message(message):
+    try:
+        print("Processing article:")
+        print(f"Category: {message.get('category', 'N/A')}")
+        print(f"Country: {message.get('country', 'N/A')}")
+        print(f"Title: {message['title']}")
+        print(f"Content: {message['content'][:100]}..." if message['content'] else "Content: N/A")
+        print(f"Publisher: {message['publisher']}")
+        print(f"Published At: {message['publishedAt']}")
+        print(f"URL: {message['url']}")
+        print("-")
 
-for message in consumer:
-    print(f"Consuming message: {message.value}")
-    index_data(message.value)
+    except Exception as e:
+        print(f"Error processing message: {e}")
+
+print(f"Subscribed to topic '{TOPIC}' and waiting for messages...")
+
+for msg in consumer:
+    try:
+        message = msg.value  # Deserialized message
+        process_message(message)
+    except Exception as e:
+        print(f"Error reading message: {e}")
